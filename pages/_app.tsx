8c9d1327990424f82efb87afd4d57a3a7737631a
MarcusWentz <markzcollege404@gmail.com>
2022-11-02 09:43:30 -0400

    Template page RainbowKit

---
+++ pages/_app.tsx
@@ -0,0 +1,78 @@
+import '../styles/global.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import type { AppProps } from 'next/app';
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  connectorsForWallets,
+  darkTheme,
+  Chain
+} from '@rainbow-me/rainbowkit';
+import { argentWallet, trustWallet } from '@rainbow-me/rainbowkit/wallets';
+import { chain, createClient, configureChains, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+// import { Chain, getDefaultWallets,darkTheme } from '@rainbow-me/rainbowkit';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+
+
+const shardeumLiberty8081: Chain = {
+  id: 8081,
+  name: 'Shardeum',
+  network: 'SHM',
+  iconUrl: 'https://example.com/icon.svg',
+  iconBackground: '#fff',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Shardeum',
+    symbol: 'SHM',
+  },
+  rpcUrls: {
+    default: 'https://liberty20.shardeum.org/',
+  },
+  blockExplorers: {
+    default: { name: 'ShardeumExplorer', url: 'https://explorer-liberty20.shardeum.org/' },
+    // etherscan: { name: 'SnowTrace', url: 'https://snowtrace.io' },
+  },
+  testnet: false,
+};
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [shardeumLiberty8081],
+  [publicProvider()]
+);
+
+const { wallets } = getDefaultWallets({
+  appName: 'RainbowKit Mint NFT Demo',
+  chains,
+});
+
+const demoAppInfo = {
+  appName: 'RainbowKit Mint NFT Demo',
+};
+
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: 'Other',
+    wallets: [argentWallet({ chains }), trustWallet({ chains })],
+  },
+]);
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider theme={darkTheme()} appInfo={demoAppInfo} chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default MyApp;
